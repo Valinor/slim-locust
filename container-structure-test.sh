dd096929371e2832679b1cbfa89116cea24645bb
Valinor <Valinor@users.noreply.github.com>
2021-11-04 17:56:32 +0100

    Add test to images

---
+++ container-structure-test.sh
@@ -0,0 +1,11 @@
+set -eu
+
+if [[ $# -lt 1 ]] ; then
+    echo $0 should have one argument : ex $0 {version}
+    exit 1
+fi
+for dockerfile in $(ls *.dockerfile)
+do 
+docker pull damballa/locust:$1-$(echo $dockerfile|cut -d"." -f1)
+container-structure-test test --image damballa/locust:$1-$(echo $dockerfile|cut -d"." -f1) --config locust-image-test.yaml
+done
