0e2eb71b0f5c6a6b2b7da4b296a36881b9c47c56
Valinor <Valinor@users.noreply.github.com>
2021-11-19 09:01:42 +0100

    Add buildx to action

---
+++ scripts/build_all_buildx.sh
@@ -0,0 +1,11 @@
+set -eu
+
+if [[ $# -lt 2 ]] ; then
+    echo $0 should have two argument : ex $0 {docker repository} {version}
+    exit 1
+fi
+set -eux
+for dockerfile in ${@:3} 
+do 
+	docker buildx build --platform linux/amd64,linux/arm64 --tag $1/locust:$2-$(echo $dockerfile|cut -d"." -f1) . -f $dockerfile
+done
